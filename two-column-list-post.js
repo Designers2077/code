695d933db2b4dce28a2ebe88ef5f14c3bda61f78
Designers2077 <86920794+Designers2077@users.noreply.github.com>
2026-01-05 11:03:07 +0545

    Update two-column-list-post.js

--- two-column-list-post.js
+++ two-column-list-post.js
@@ -1,4 +1,3 @@
-/* news-fetcher.js */
 function fetchNews(labelName, targetId, postCount) {
   var scriptURL = '/feeds/posts/default/-/' + encodeURIComponent(labelName) + '?alt=json-in-script&callback=callback_' + targetId.replace(/-/g, '_') + '&max-results=' + postCount;
   var scriptTag = document.createElement('script');
@@ -9,18 +8,29 @@ function fetchNews(labelName, targetId, postCount) {
     var targetDiv = document.getElementById(targetId);
     var contentHtml = '';
     
-    if (data.feed.entry) {
+    if (data.feed && data.feed.entry) {
       for (var i = 0; i < data.feed.entry.length; i++) {
         var entry = data.feed.entry[i];
         var title = entry.title.$t;
-        var postUrl = entry.link.find(l => l.rel === 'alternate').href;
+        var postUrl = "";
+        
+        // सुरक्षित तरिकाले लिंक खोज्ने
+        for (var k = 0; k < entry.link.length; k++) {
+          if (entry.link[k].rel == 'alternate') {
+            postUrl = entry.link[k].href;
+            break;
+          }
+        }
+        
         var thumbUrl = entry.media$thumbnail ? entry.media$thumbnail.url.replace('s72-c', 'w400-h280-c') : 'https://via.placeholder.com/140x90';
         
-        contentHtml += `<div class="custom-post-item">
-                          <img src="${thumbUrl}" alt="${title}">
-                          <a href="${postUrl}">${title}</a>
-                        </div>`;
+        contentHtml += '<div class="custom-post-item">' +
+                          '<img src="' + thumbUrl + '" alt="' + title + '">' +
+                          '<a href="' + postUrl + '">' + title + '</a>' +
+                       '</div>';
       }
+    } else {
+      contentHtml = '<p>No posts found in this category.</p>';
     }
     targetDiv.innerHTML = contentHtml;
   };
